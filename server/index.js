5c5d6dfe3554d22db391691e608271a050ce7a0e
rawasaditya <rawasaditya@gmail.com>
2023-06-16 06:15:55 +0530

    Added User Model

--- server/index.js
+++ server/index.js
@@ -7,17 +7,17 @@ import morgan from "morgan";
 import path from "path";
 import helmet from "helmet";
 import { fileURLToPath } from "url";
-
+import { register } from "./controllers/auth.js";
 /* Configurations */
+morgan("tiny");
+dotenv.config();
 const __fileName = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__fileName);
-
 const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
-
 /* FILE STORAGE */
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -27,5 +27,17 @@ const storage = multer.diskStorage({
     cb(null, file.originalname);
   },
 });
-
 const upload = multer({ storage: storage });
+// ROUTES WITH FILES
+app.post("/auth/register", upload.single("picture"), register);
+
+// MONGOOSE SETUP
+const PORT = process.env.API_PORT;
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then((data) => {
+    app.listen(PORT, () => console.log(`SERVER PORT: ${PORT}`));
+  })
+  .catch((err) => {
+    console.log(err);
+  });
---
+++ server/models/User.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 20,
+    },
+    lastName: {
+      type: String,
+      required: false,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    picturePath: {
+      type: String,
+      require: false,
+    },
+    location: String,
+    occupation: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+const User = mongoose.model("User", UserSchema);
+export default User;
