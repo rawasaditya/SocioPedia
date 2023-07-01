6791646a3163ce1b0ef5aee8650887951df66224
el <elidakirigo@gmail.com>
2023-07-01 09:08:01 +0300

    adding the search drop down #23

--- frontend/src/Components/SearchDropDown/index.jsx
+++ frontend/src/Components/SearchDropDown/index.jsx
@@ -1,13 +1,11 @@
 import { Box } from '@mui/material'
 import React from 'react'
 function SearchDropDown(props) {
-
-
 	return (
 		<div>
-		<Box sx={{pt:1, pb:1}}>example</Box>
-		<Box sx={{pt:1, pb:1}}>hello</Box>
-		<Box sx={{pt:1, pb:1}}>grEAT1</Box>
+			<Box sx={{ pt: 1, pb: 1 }}>example</Box>
+			<Box sx={{ pt: 1, pb: 1 }}>hello</Box>
+			<Box sx={{ pt: 1, pb: 1 }}>grEAT1</Box>
 		</div>
 
 	)
